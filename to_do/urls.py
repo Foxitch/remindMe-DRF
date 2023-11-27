669ff608b13a0b2450c0b8526f01ceb39fe3bd5e
maksim.lazko <m.lazko@naveksoft.com>
2023-11-27 13:44:20 +0300

    refactored todo view set

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,19 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from to_do.views import (BoardViewSet, TodoCreateApiView, TodoDeleteApiView,
-                         TodoListApiView, TodoListUndoneApiView,
-                         TodoUpdateApiView)
+from to_do.views import BoardViewSet, TodoViewSet
+
 
 router = routers.SimpleRouter()
 router.register(prefix=r'board', viewset=BoardViewSet)
+router.register(prefix=r'to_do', viewset=TodoViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('todo-create/', TodoCreateApiView.as_view()),
-    path('todo-list/<int:board>/', TodoListApiView.as_view()),
-    path('todo-list-undone/<int:board>/', TodoListUndoneApiView.as_view()),
-    path('todo-delete/<int:pk>/', TodoDeleteApiView.as_view()),
-    path('todo-update/<int:pk>/', TodoUpdateApiView.as_view()),
 ]
--- to_do/views.py
+++ to_do/views.py
@@ -1,6 +1,5 @@
-from django.db.models import QuerySet
 from overrides import override
-from rest_framework import generics, permissions
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -20,68 +19,31 @@ class BoardViewSet(ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     @override
-    def list(self, request, *args, **kwargs):
+    def list(self, request, *args, **kwargs) -> Response:
         queryset = self.filter_queryset(self.get_queryset())
         data = self.get_serializer(queryset, many=True).data
         for board_data in data:
             board_id = board_data['id']
             todo_count = ToDoList.objects.filter(board_id=board_id).count()
             board_data['todo_count'] = todo_count
-        return Response(data)
+        return Response(data=data)
 
 
-class TodoCreateApiView(generics.CreateAPIView):
+class TodoViewSet(ModelViewSet):
     """
-    API for to do creating
-    permission: -IsAdmin
+    API for CRUD to_do, GET has filter by status
+    permission: -IsOwnerOrAdminPermission
     """
 
-    serializer_class = ToDoListApiViewSerializer
-    permission_classes = (permissions.IsAdminUser,)
-    queryset = ToDoList.objects.all()
-
-
-class TodoListApiView(generics.ListAPIView):
-    """
-    API for getting list of all ToDos
-    permission: -IsAuth
-    """
-    serializer_class = ToDoListApiViewSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def get_queryset(self) -> QuerySet:
-        queryset = ToDoList.objects.filter(board=self.kwargs['board'])
-        return queryset
-
-
-class TodoListUndoneApiView(generics.ListAPIView):
-    """
-    API for getting list of undones todos
-    permission: -IsAuth
-    """
-    serializer_class = ToDoListApiViewSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def get_queryset(self) -> QuerySet:
-        queryset = ToDoList.objects.filter(done=self.kwargs['board']).filter(done=False)
-        return queryset
-
-
-class TodoDeleteApiView(generics.DestroyAPIView):
-    """
-    API for deleting todos
-    permission: -IsAdmin
-    """
-    serializer_class = ToDoListApiViewSerializer
-    permission_classes = (permissions.IsAdminUser,)
-    queryset = ToDoList.objects.all()
-
-
-class TodoUpdateApiView(generics.RetrieveUpdateAPIView):
-    """
-    API for update todos
-    permission: -IsOwnerOrStaff
-    """
     serializer_class = ToDoListApiViewSerializer
     permission_classes = (IsOwnerOrAdminPermission,)
     queryset = ToDoList.objects.all()
+
+    @override
+    def list(self, request, *args, **kwargs) -> Response:
+        if request.query_params.get('done') == 'true':
+            queryset = ToDoList.objects.filter(done=True)
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+        data = self.get_serializer(queryset, many=True).data
+        return Response(data=data)
